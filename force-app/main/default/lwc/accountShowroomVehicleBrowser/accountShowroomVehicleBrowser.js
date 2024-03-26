4b456762e9483a414e531fdf5ff21221fe0b4483
Adrianna Zajac <adrianna.maria.zajac@gmail.com>
2024-03-26 10:48:40 +0100

    Added LWC for Home page.

--- force-app/main/default/lwc/accountShowroomVehicleBrowser/accountShowroomVehicleBrowser.js
+++ force-app/main/default/lwc/accountShowroomVehicleBrowser/accountShowroomVehicleBrowser.js
@@ -3,16 +3,10 @@ import getAllAccounts from '@salesforce/apex/AccountShowroomVehicleBrowserContro
 import getShowroomsByAccountId from '@salesforce/apex/AccountShowroomVehicleBrowserController.getShowroomsByAccountId';
 import getVehiclesByShowroomId from '@salesforce/apex/AccountShowroomVehicleBrowserController.getVehiclesByShowroomId';
 
-const COLUMNS = [
-    { label: 'Serial number', fieldName: 'Name', type: 'text' },
-    { label: 'Brand', fieldName: 'Brands__c', type: 'text' },
-    { label: 'Model', fieldName: 'Model__c', type: 'text' }
-];
-
 export default class AccountShowroomVehicleBrowser extends LightningElement {
     accounts;
     showrooms;
-    @api vehicles;
+    vehicles;
     @track vehicleId;
     @api numberOfPages;
     numberOfPages = 0; 
@@ -32,7 +26,6 @@ export default class AccountShowroomVehicleBrowser extends LightningElement {
     get isThirdPage(){
         return this.numberOfPages === 3;
     }
-    
 
     handleButtonBackClick(event){
         this.numberOfPages--;
@@ -41,9 +34,9 @@ export default class AccountShowroomVehicleBrowser extends LightningElement {
     @wire(getAllAccounts)
     allAccounts({ error, data }) {
         if (data) {
-            this.accounts = data;
+            this.accounts = data;            
         } else if (error) {
-            // Handle error
+            console.error(error);
         }
     }
 
@@ -52,59 +45,29 @@ export default class AccountShowroomVehicleBrowser extends LightningElement {
         getShowroomsByAccountId({ accountId })
             .then(result => {
                 this.showrooms = result;
+
             })
             .catch(error => {
-                // Handle error
+                console.error(error);
             });
-            this.numberOfPages++;
-            console.log('page')
-            console.log(this.numberOfPages);
-            
+            this.numberOfPages++;          
     }
 
     handleShowroomClick(event) {
-        console.log("ddddd");
         const showroomId = event.currentTarget.dataset.id;
         getVehiclesByShowroomId({ showroomId })
             .then(result => {
                 this.vehicles = result;
             })
             .catch(error => {
-                // Handle error
+                console.error(error);
             });
             this.numberOfPages++;
     }
 
-    get columns() {
-        return COLUMNS;
-    }
-
-    // handleRowAction(event){
-    //     const vehicleId = event.detail.row.Id;
-    //     console.log("Action");
-    //     console.log(vehicleId);
-    // }
-
     handleVehicleClick(event){
         const vehicleId = event.currentTarget.dataset.id;
         this.vehicleId = vehicleId;
-        console.log("VehicleId: " + this.vehicleId);
         this.numberOfPages++;
     }
-
-    // handleInterestChange(event) {
-    //     const vehicleId = event.currentTarget.dataset.id;
-    //     const interested = event.target.checked;
-
-    //     // Update 'interested' field for the vehicle
-    //     updateVehicleInterest({ vehicleId, interested })
-    //         .then(result => {
-    //             // Handle success
-    //             console.log('Vehicle interest updated successfully.');
-    //         })
-    //         .catch(error => {
-    //             // Handle error
-    //             console.error('Error updating vehicle interest:', error);
-    //         });
-    // }
 }
--- force-app/main/default/lwc/vehicleDetails/vehicleDetails.js
+++ force-app/main/default/lwc/vehicleDetails/vehicleDetails.js
@@ -1,9 +1,9 @@
 import { LightningElement, api } from 'lwc';
 
-
 export default class VehicleDetails extends LightningElement {
     @api recordId;
 
-
+    saveInterest(e) {
+        this.template.querySelector('lightning-record-edit-form').submit();
+    }
 }
-
