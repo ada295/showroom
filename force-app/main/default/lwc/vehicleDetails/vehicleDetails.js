128d30fbc4a5353d3d11df859cbf93cf9bfa4f3d
Adrianna Zajac <adrianna.maria.zajac@gmail.com>
2024-03-26 23:19:32 +0100

    Added comments and test for getAllAccounts alphabetically.

--- force-app/main/default/lwc/vehicleDetails/vehicleDetails.js
+++ force-app/main/default/lwc/vehicleDetails/vehicleDetails.js
@@ -1,7 +1,10 @@
 import { LightningElement, api } from 'lwc';
 
+
 export default class VehicleDetails extends LightningElement {
     @api recordId;
+
+    handleSuccess() {}
 
     saveInterest(e) {
         this.template.querySelector('lightning-record-edit-form').submit();
