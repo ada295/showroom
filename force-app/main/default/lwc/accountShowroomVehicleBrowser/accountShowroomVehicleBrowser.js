c6c19a8785692df974e339bba2b26ba275071b4b
Adrianna Zajac <adrianna.maria.zajac@gmail.com>
2024-03-27 09:59:38 +0100

    Added jsdocs.

--- force-app/main/default/lwc/accountShowroomVehicleBrowser/accountShowroomVehicleBrowser.js
+++ force-app/main/default/lwc/accountShowroomVehicleBrowser/accountShowroomVehicleBrowser.js
@@ -1,3 +1,14 @@
+/**
+ * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+ * @date 25/03/2024
+ * @description This class contains methods for managing events related to accounts, showrooms and vehicles.
+ * 
+ * @param accounts List of all accounts.
+ * @param showrooms List of showrooms which are connected with chosen account.
+ * @param vehicles List of vehicles which are connected wirh chosen showroom.
+ * @param vehicleId Id of chosen vehicle.
+ * @param numberOfPages Number of the current page.
+ */
 import { LightningElement, wire, api, track } from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountShowroomVehicleBrowserController.getAllAccounts';
 import getShowroomsByAccountId from '@salesforce/apex/AccountShowroomVehicleBrowserController.getShowroomsByAccountId';
@@ -8,29 +19,52 @@ export default class AccountShowroomVehicleBrowser extends LightningElement {
     showrooms;
     vehicles;
     @track vehicleId;
-    @api numberOfPages;
-    numberOfPages = 0; 
-
+    @api numberOfPages = 0;
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method verifies if numberOfPages equals 0.
+     */
     get isZeroPage(){
         return this.numberOfPages === 0;
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method verifies if numberOfPages equals 1.
+     */
     get isFirstPage(){
         return this.numberOfPages === 1;
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method verifies if numberOfPages equals 2.
+     */
     get isSecondPage(){
         return this.numberOfPages === 2;
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method verifies if numberOfPages equals 3.
+     */
     get isThirdPage(){
         return this.numberOfPages === 3;
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method is fired when BackButton is clicked and decrements numberOfPages.
+     */
     handleButtonBackClick(event){
         this.numberOfPages--;
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method get all accounts.
+     */
     @wire(getAllAccounts)
     allAccounts({ error, data }) {
         if (data) {
@@ -39,7 +73,13 @@ export default class AccountShowroomVehicleBrowser extends LightningElement {
             console.error(error);
         }
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method is fired when account record is clicled, gets showrooms by accountId and increments number of pages.
+     * 
+     * @param accountId Id of chosen account.
+     */
     handleAccountClick(event) {
         const accountId = event.currentTarget.dataset.id;
         getShowroomsByAccountId({ accountId })
@@ -52,7 +92,13 @@ export default class AccountShowroomVehicleBrowser extends LightningElement {
             });
             this.numberOfPages++;          
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method is fired when showroom record is clicled, gets vehocles by showroomId and increments number of pages.
+     * 
+     * @param showroomId Id of chosen showroom.
+     */
     handleShowroomClick(event) {
         const showroomId = event.currentTarget.dataset.id;
         getVehiclesByShowroomId({ showroomId })
@@ -64,7 +110,13 @@ export default class AccountShowroomVehicleBrowser extends LightningElement {
             });
             this.numberOfPages++;
     }
-
+    /**
+     * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+     * @date 25/03/2024
+     * @description The method is fired when vehicle record is clicled and increments number of pages.
+     * 
+     * @param vehicleId Id of chosen vehicle.
+     */
     handleVehicleClick(event){
         const vehicleId = event.currentTarget.dataset.id;
         this.vehicleId = vehicleId;
--- force-app/main/default/lwc/vehicleDetails/vehicleDetails.js
+++ force-app/main/default/lwc/vehicleDetails/vehicleDetails.js
@@ -1,5 +1,11 @@
+/**
+ * @author Adrianna Zajac <adrianna.zajac@accenture.com>
+ * @date 25/03/2024
+ * @description This class provides methods for handling vehicle interest checkbox.
+ * 
+ * @param recordId Id of chosen vehicle.
+ */
 import { LightningElement, api } from 'lwc';
-
 
 export default class VehicleDetails extends LightningElement {
     @api recordId;
